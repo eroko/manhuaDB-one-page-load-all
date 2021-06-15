0203187a0f5568220ce4a28e079dbdaea766f74e
eroko <cloverqi@gmail.com>
2021-06-15 23:54:59 +0800

    Fix Script And Remove Movie Ads, Still Have Some Bugs to Fix

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         manhuaDB Show All Imgs
-// @version      0.1
+// @version      0.2
 // @description  This script will load full chapter in one page. Default speed is 4s per page. Only for manhuaDB.
 // @author       Eroko
 // @match        https://www.manhuadb.com/manhua/*
@@ -9,9 +9,12 @@
 
 (function () {
     'use strict';
+    console.log(img_data_arr)
+    $(".ad_1_wrap").remove();
+    $(".ad_2_wrap").remove();
     for (let i = page; i < img_data_arr.length; i++) {
         setTimeout(function () {
-            let url3 = img_host + "/static/" + p_ccid + "/" + p_id + "/" + img_data_arr[i]["img_webp"]
+            let url3 = img_host + "/static/" + p_ccid + "/" + p_id + "/" + img_data_arr[i]["img"]
             let k = i + 1
             var html = '<div><img src=' + url3 + '></div><div><h4>第' + k + '页</h4>></div>>'
             $(".comic-viewer-toc").append(html);
